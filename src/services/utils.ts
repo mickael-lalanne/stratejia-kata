c578a9ede33bf38ea266a4a12923d60580c1bc84
Mickaël Lalanne <mickael.lalanne03@gmail.com>
2023-11-02 09:32:40 -0400

    Show all combinations on Readable version

---
+++ src/services/utils.ts
@@ -0,0 +1,21 @@
+/**
+ * Get words from the wordlist.txt file
+ * @returns {string[]} an array containing all the dictionnary words
+ */
+export async function getDictionnaryWords(): Promise<string[]> {
+    // First, read the wordlist.txt file using the fetch api
+    // cf https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    const wordsResponse: Response = await fetch('wordlist.txt');
+
+    // If an error has occured while reading the file, return an empty array
+    if (!wordsResponse.ok) {
+        return [];
+    }
+
+    // Then, get the text content
+    const textWords: string = await wordsResponse.text();
+
+    // Finally, convert it to a string array
+    // \n means a line break
+    return textWords.split('\n');
+}
--- src/views/ReadableView.vue
+++ src/views/ReadableView.vue
@@ -1,46 +1,86 @@
 <template>
     <main>
         <div>Readable view.</div>
-        <div>All dictionnary words :</div>
+        <div>All combinations :</div>
         <div class="words-container">
-            {{ dictionnaryWords }}
+            {{ allCombinations }}
         </div>
     </main>
 </template>
 
 <script lang="ts">
+import { getDictionnaryWords } from '@/services/utils';
+
+const WORD_MIN_SIZE: number = 2;
+
 export default {
     name: 'ReadableView',
     data() {
         return {
             dictionnaryWords: [] as string[],
+            allCombinations: [] as string[]
         };
     },
     async beforeMount() {
-        this.dictionnaryWords = await this.getDictionnaryWords();
+        this.dictionnaryWords = await getDictionnaryWords();
+
+        this.findCombinedWords();
     },
     methods: {
-        /**
-         * Get words from the wordlist.txt file
-         * @returns {string[]} an array containing all the dictionnary words
-         */
-        async getDictionnaryWords(): Promise<string[]> {
-            // First, read the wordlist.txt file using the fetch api
-            // cf https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-            const wordsResponse: Response = await fetch('wordlist.txt');
+        findCombinedWords(): void {
+            const sixLettersWords: string[] = this.dictionnaryWords.filter(word => word.length === 6);
 
-            // If an error has occured while reading the file, return an empty array
-            if (!wordsResponse.ok) {
-                return [];
-            }
+            // For each 6 letters words in the dictionnary
+            sixLettersWords.forEach((sixLettersWord) => {
+                // Get all his prefixes
+                const wordPrefixes: string [] = [];
 
-            // Then, get the text content
-            const textWords: string = await wordsResponse.text();
+                for (let index = 0; index < sixLettersWord.length - WORD_MIN_SIZE; index++) {
+                    const potentialPrefix: string = sixLettersWord.substring(0, index + WORD_MIN_SIZE);
 
-            // Finally, convert it to a string array
-            // \n means a line break
-            return textWords.split('\n');
-        },
+                    const isPrefixInDictionnary: boolean = this.dictionnaryWords.includes(potentialPrefix);
+
+                    if (isPrefixInDictionnary) {
+                        wordPrefixes.push(potentialPrefix);
+                    }
+                }
+
+                // Get all his suffixes
+                const wordSuffixes: string [] = [];
+
+                for (let index = 0; index < sixLettersWord.length - WORD_MIN_SIZE; index++) {
+                    const potentialSuffix: string = sixLettersWord.substring(1, index + 1 + WORD_MIN_SIZE);
+
+                    const isSuffixInDictionnary: boolean = this.dictionnaryWords.includes(potentialSuffix);
+
+                    if (isSuffixInDictionnary) {
+                        wordSuffixes.push(potentialSuffix);
+                    }
+                }
+
+                // Todo : use promises to parralel
+
+                // Check for combinations
+                wordPrefixes.forEach(prefix => {
+                    wordSuffixes.forEach(suffix => {
+                        const combination: string = prefix + suffix;
+
+                        // The combination text displayed to the user, formated like : prefix + suffix => combination
+                        // We use toLowerCase() to avoid duplicates
+                        const formatedCombination: string = `${prefix.toLowerCase()} + ${suffix.toLowerCase()} => ${combination}`;
+
+                        if (
+                            // If the combination is a real word in the dictionnary
+                            this.dictionnaryWords.includes(combination) &&
+                            // AND if the combination hasn't been added yet
+                            !this.allCombinations.includes(formatedCombination)
+                        ) {
+                            this.allCombinations.push(formatedCombination);
+                        }
+                    })
+                });
+            });
+        }
     },
 };
 </script>
