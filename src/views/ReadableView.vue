6d368995e7b25227e24e11d10e5661f1b9bcd92d
Mickaël Lalanne <mickael.lalanne03@gmail.com>
2023-11-02 10:12:46 -0400

    Refacto : use promises to improve perfs + add methods to make the code more readable

--- src/views/ReadableView.vue
+++ src/views/ReadableView.vue
@@ -24,20 +24,46 @@ export default {
     async beforeMount() {
         this.dictionnaryWords = await getDictionnaryWords();
 
-        this.findCombinedWords();
+        this.parseDictionnaryWords();
     },
     methods: {
-        findCombinedWords(): void {
+        /**
+         * Called once all word have been retrieved from the dictionnary file
+         * For all 6 letters words, check if we can have a combination of 2 words
+         */
+        parseDictionnaryWords(): void {
             const sixLettersWords: string[] = this.dictionnaryWords.filter(word => word.length === 6);
 
             // For each 6 letters words in the dictionnary
             sixLettersWords.forEach((sixLettersWord) => {
-                // Get all his prefixes
+                // Get all prefixes and suffixes in parallel
+                Promise.all([
+                    this.findAllPrefixes(sixLettersWord),
+                    this.findAllSuffixes(sixLettersWord)
+                ]).then(results => {
+                        // Once we got all the prefixes and suffixes, check for combinations
+                        const wordPrefixes = results[0];
+                        const wordSuffixes = results[1];
+
+                        this.findForCombinations(wordPrefixes, wordSuffixes);
+                }).catch(err => {
+                    console.error(err);
+                });
+            });
+        },
+        /**
+         * Find all word prefixes for a six letters word 
+         * @param {string} sixLettersWord the word we have to find all his prefixes
+         * @returns {Promise<string[]>} all prefixes contained in the given word
+         */
+        findAllPrefixes(sixLettersWord: string): Promise<string[]> {
+            return new Promise((resolve) => {
                 const wordPrefixes: string [] = [];
 
                 for (let index = 0; index < sixLettersWord.length - WORD_MIN_SIZE; index++) {
                     const potentialPrefix: string = sixLettersWord.substring(0, index + WORD_MIN_SIZE);
 
+                    // Check if the suffix is a real word
                     const isPrefixInDictionnary: boolean = this.dictionnaryWords.includes(potentialPrefix);
 
                     if (isPrefixInDictionnary) {
@@ -45,12 +71,22 @@ export default {
                     }
                 }
 
-                // Get all his suffixes
+                resolve(wordPrefixes);
+            });
+        },
+        /**
+         * Find all word suffixes for a six letters word 
+         * @param {string} sixLettersWord the word we have to find all his suffixes
+         * @returns {Promise<string[]>} all suffixes contained in the given word
+         */
+        findAllSuffixes(sixLettersWord: string): Promise<string[]> {
+            return new Promise((resolve) => {
                 const wordSuffixes: string [] = [];
 
                 for (let index = 0; index < sixLettersWord.length - WORD_MIN_SIZE; index++) {
                     const potentialSuffix: string = sixLettersWord.substring(1, index + 1 + WORD_MIN_SIZE);
 
+                    // Check if the suffix is a real word
                     const isSuffixInDictionnary: boolean = this.dictionnaryWords.includes(potentialSuffix);
 
                     if (isSuffixInDictionnary) {
@@ -58,29 +94,34 @@ export default {
                     }
                 }
 
-                // Todo : use promises to parralel
-
-                // Check for combinations
-                wordPrefixes.forEach(prefix => {
-                    wordSuffixes.forEach(suffix => {
-                        const combination: string = prefix + suffix;
-
-                        // The combination text displayed to the user, formated like : prefix + suffix => combination
-                        // We use toLowerCase() to avoid duplicates
-                        const formatedCombination: string = `${prefix.toLowerCase()} + ${suffix.toLowerCase()} => ${combination}`;
-
-                        if (
-                            // If the combination is a real word in the dictionnary
-                            this.dictionnaryWords.includes(combination) &&
-                            // AND if the combination hasn't been added yet
-                            !this.allCombinations.includes(formatedCombination)
-                        ) {
-                            this.allCombinations.push(formatedCombination);
-                        }
-                    })
-                });
+                resolve(wordSuffixes);
             });
-        }
+        },
+        /**
+         * Find real words with given prefixes and suffixes
+         * @param {string[]} prefixes array containing real words we have to use as prefixes 
+         * @param {string[]} suffixes array containing real words we have to use as suffixes 
+         */
+        findForCombinations(prefixes: string[], suffixes: string[]): void {
+            prefixes.forEach(prefix => {
+                suffixes.forEach(suffix => {
+                    const combination: string = prefix + suffix;
+
+                    // The combination text displayed to the user, formated like : prefix + suffix => combination
+                    // We use toLowerCase() to avoid duplicates
+                    const formatedCombination: string = `${prefix.toLowerCase()} + ${suffix.toLowerCase()} => ${combination}`;
+
+                    if (
+                        // If the combination is a real word in the dictionnary
+                        this.dictionnaryWords.includes(combination) &&
+                        // AND if the combination hasn't been added yet
+                        !this.allCombinations.includes(formatedCombination)
+                    ) {
+                        this.allCombinations.push(formatedCombination);
+                    }
+                })
+            });
+}
     },
 };
 </script>
